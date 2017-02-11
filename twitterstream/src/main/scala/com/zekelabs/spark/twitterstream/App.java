61b9479709d14035de04979bc04ebee3c9da8059
awantik <awantikdas@gmail.com>
2017-02-11 21:35:18 +0530

    Spark Scala Twitter Streaming App

---
+++ twitterstream/src/main/scala/com/zekelabs/spark/twitterstream/App.java
@@ -0,0 +1,13 @@
+package com.zekelabs.spark.twitterstream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ twitterstream/src/main/scala/com/zekelabs/spark/twitterstream/MyTwApp.scala
@@ -0,0 +1,86 @@
+package com.zekelabs.spark.twitterstream
+
+import org.apache.spark.streaming.{ Seconds, StreamingContext }
+import org.apache.spark.SparkContext._
+import org.apache.spark.streaming.twitter._
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.{ SparkContext, SparkConf }
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.flume._
+
+/**
+ * A Spark Streaming application that receives tweets on certain 
+ * keywords from twitter datasource and find the popular hashtags
+ * 
+ * Arguments: <comsumerKey> <consumerSecret> <accessToken> <accessTokenSecret> <keyword_1> ... <keyword_n>
+ * <comsumerKey>        - Twitter consumer key 
+ * <consumerSecret>     - Twitter consumer secret
+ * <accessToken>        - Twitter access token
+ * <accessTokenSecret>  - Twitter access token secret
+ * <keyword_1>          - The keyword to filter tweets
+ * <keyword_n>          - Any number of keywords to filter tweets
+ * 
+ */
+
+object MyTwApp {
+  val conf = new SparkConf().setMaster("local[4]").setAppName("Spark Streaming - PopularHashTags")
+  val sc = new SparkContext(conf)
+
+  def main(args: Array[String]) {
+
+    sc.setLogLevel("WARN")
+
+    val Array(consumerKey, consumerSecret, accessToken, accessTokenSecret) = args.take(4)
+    val filters = args.takeRight(args.length - 4)
+
+    // Set the system properties so that Twitter4j library used by twitter stream
+    // can use them to generat OAuth credentials
+    System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
+    System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
+    System.setProperty("twitter4j.oauth.accessToken", accessToken)
+    System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+
+    // Set the Spark StreamingContext to create a DStream for every 5 seconds
+    val ssc = new StreamingContext(sc, Seconds(5)) // Wait for 5 secs & then create RDD
+    // Pass the filter keywords as arguements
+
+    //  val stream = FlumeUtils.createStream(ssc, args(0), args(1).toInt)  
+    val stream = TwitterUtils.createStream(ssc, None, filters) 
+    // Creating a DStream of selected filters
+    
+    
+    // Split the stream on space and extract hashtags 
+    val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+    // hashTags consists of all #tags & nothing else
+    
+    // Get the top hashtags over the previous 60 sec window
+    val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+      .map { case (topic, count) => (count, topic) }
+      .transform(_.sortByKey(false))
+
+    // Get the top hashtags over the previous 10 sec window
+    val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+      .map { case (topic, count) => (count, topic) }
+      .transform(_.sortByKey(false))
+
+    // print tweets in the currect DStream 
+    stream.print()
+
+    // Print popular hashtags  
+    topCounts60.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })
+    topCounts10.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+} 
+
